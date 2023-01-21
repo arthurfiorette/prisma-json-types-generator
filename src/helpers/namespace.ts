a10da66bf145dbcc8eddbda1d1a6842cb0aa45d5
arthurfiorette <arthur.fiorette@gmail.com>
2023-01-21 14:19:28 -0300

    fix: do not pollute global scope

--- src/helpers/namespace.ts
+++ src/helpers/namespace.ts
@@ -3,8 +3,27 @@ export function createNamespace(nsName: string) {
 
 declare global {
   namespace ${nsName} {}
+}
 
-  type DeepPartial<T> = T extends object ? { [P in keyof T]?: DeepPartial<T[P]> } : T;  
+// Helpers
+
+type Update<T> = T extends object ? { [P in keyof T]?: Update<T[P]> } : T;
+
+type NullableListFilter<T> = {
+  equals?: Enumerable<T> | null;
+  has?: T | null;
+  hasEvery?: Enumerable<T>;
+  hasSome?: Enumerable<T>;
+  isEmpty?: boolean;
+};
+
+export type UpdateInput<T> = {
+  set?: Enumerable<T>;
+  push?: T | Enumerable<T>;
+};
+
+export type CreateInput<T> = {
+  set?: Enumerable<T>;
 };
 
 `.trim();
