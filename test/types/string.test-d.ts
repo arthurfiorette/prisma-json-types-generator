87f806cd1651fee610d041e529bef27d0fea347f
arthurfiorette <arthur.fiorette@gmail.com>
2023-08-15 00:00:46 -0300

    test: added string literal tests

---
+++ test/types/string.test-d.ts
@@ -0,0 +1,22 @@
+import { expectNotType, expectType } from 'tsd';
+import { Model } from '../target/string/index';
+
+declare global {
+  export namespace PStringJson {
+    type WithType = 'C' | 'D';
+  }
+}
+
+expectType<Model>({
+  id: 0,
+  untyped: '' as string,
+  typed: 'C' as PStringJson.WithType,
+  literal: 'A' as 'A' | 'B'
+});
+
+expectNotType<Model>({
+  id: 0,
+  untyped: 'Mesquita' as string,
+  typed: 'D' as string,
+  literal: 'D' as string
+});
--- test/types/unknown.test-d.ts
+++ test/types/unknown.test-d.ts
@@ -3,10 +3,21 @@ import { Model } from '../target/unknown/index';
 
 expectType<Model>({
   id: 0,
-  field: {} as unknown
+  field: {} as unknown,
+  str: '' as string,
+  int: 0 as number,
 });
 
 expectNotType<Model>({
   id: 0,
-  field: {} as any
+  field: {} as any,
+  str: {} as unknown,
+  int: 0 as unknown,
+});
+
+expectNotType<Model>({
+  id: 0,
+  field: {} as any,
+  str: {} as any,
+  int: 0 as any
 });
