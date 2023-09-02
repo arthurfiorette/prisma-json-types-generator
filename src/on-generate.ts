21eac421ad9fd4df898d00c694bd0f436da7974b
arthurfiorette <arthur.fiorette@gmail.com>
2023-09-02 16:05:39 -0300

    fix: print errors to stdout

--- src/on-generate.ts
+++ src/on-generate.ts
@@ -9,41 +9,45 @@ import { buildTypesFilePath } from './util/source-path';
 
 /** Runs the generator with the given options. */
 export async function onGenerate(options: GeneratorOptions) {
-  const prismaClient = findPrismaClientGenerator(options.otherGenerators);
+  try {
+    const prismaClient = findPrismaClientGenerator(options.otherGenerators);
 
-  const config = parseConfig(options.generator.config);
+    const config = parseConfig(options.generator.config);
 
-  const clientOutput = buildTypesFilePath(
-    prismaClient.output.value,
-    config.clientOutput,
-    options.schemaPath
-  );
+    const clientOutput = buildTypesFilePath(
+      prismaClient.output.value,
+      config.clientOutput,
+      options.schemaPath
+    );
 
-  const writer = new DeclarationWriter(clientOutput, config);
+    const writer = new DeclarationWriter(clientOutput, config);
 
-  // Reads the prisma declaration file content.
-  await writer.load();
+    // Reads the prisma declaration file content.
+    await writer.load();
 
-  const tsSource = ts.createSourceFile(
-    writer.filepath,
-    writer.content,
-    ts.ScriptTarget.ESNext,
-    true,
-    ts.ScriptKind.TS
-  );
+    const tsSource = ts.createSourceFile(
+      writer.filepath,
+      writer.content,
+      ts.ScriptTarget.ESNext,
+      true,
+      ts.ScriptKind.TS
+    );
 
-  const prismaModels = extractPrismaModels(options.dmmf);
+    const prismaModels = extractPrismaModels(options.dmmf);
 
-  // Handles the prisma namespace.
-  tsSource.forEachChild((child) => {
-    try {
-      if (child.kind === ts.SyntaxKind.ModuleDeclaration) {
-        handlePrismaModule(child as ts.ModuleDeclaration, writer, prismaModels, config);
+    // Handles the prisma namespace.
+    tsSource.forEachChild((child) => {
+      try {
+        if (child.kind === ts.SyntaxKind.ModuleDeclaration) {
+          handlePrismaModule(child as ts.ModuleDeclaration, writer, prismaModels, config);
+        }
+      } catch (error) {
+        console.error(error);
       }
-    } catch (error) {
-      console.log(error);
-    }
-  });
+    });
 
-  await writer.save();
+    await writer.save();
+  } catch (error) {
+    console.error(error);
+  }
 }
