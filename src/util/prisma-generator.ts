b3074396db7d145cbb14c0b93d7e18b7aea4b14e
Emanuel <emanuel-caue@hotmail.com>
2024-01-25 15:21:49 -0300

    fix: find client generator by provider instead of the name (#240)

--- src/util/prisma-generator.ts
+++ src/util/prisma-generator.ts
@@ -6,7 +6,7 @@ import { PrismaJsonTypesGeneratorError } from './error';
  * an error.
  */
 export function findPrismaClientGenerator(generators: GeneratorConfig[]) {
-  const options = generators.find((g) => g.name === 'client');
+  const options = generators.find((g) => g.provider.value === 'prisma-client-js');
 
   if (!options) {
     throw new PrismaJsonTypesGeneratorError(
