594b9a32ff2257b7c5866f08103d494b0eeb3d90
Arthur Fiorette <me@arthur.place>
2024-09-29 22:03:11 -0300

    fix: fail CI if test is broken

--- scripts/test.sh
+++ scripts/test.sh
@@ -3,17 +3,15 @@
 # Gets all files in test/schemas (Probably there's a better way to do this)
 FILES=$(ls -l test/schemas | awk '{print $9}' | awk -F '.' '{print $1}')
 
+# Generates each prisma schema
 for file in $FILES; do
-  # Runs prisma generate hiding stdout
-  pnpm prisma generate \
-    --schema test/schemas/$file.prisma >/dev/null &&
-    pnpm tsd \
-      -f test/types/$file.test-d.ts \
-      -t . \
-      --show-diff &&
-    echo "✅ $file" \
-    &
+  pnpm prisma generate --schema test/schemas/$file.prisma > /dev/null &
 done
 
-# Waits for all tests to finish
+# Waits for generation
 wait
+
+# Runs tests sequentially
+for file in $FILES; do
+  pnpm tsd -f test/types/$file.test-d.ts -t . --show-diff && echo "✅ $file"
+done
