5e7f2671df865f3f9b5f703f99ab0f220246203d
Arthur Fiorette <me@arthur.place>
2024-09-29 23:08:39 -0300

    feat: use biomejs & improved configs

--- .prettierrc.js
+++
@@ -1,5 +0,0 @@
-// https://github.com/ArthurFiorette/prettier-config
-
-module.exports = require('@arthurfiorette/prettier-config')({
-  // Any options here will override @arthurfiorette/prettier-config defaults
-});
