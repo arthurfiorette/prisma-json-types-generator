29af8434fb7a1ed92b44820f8886325b655c2cb2
Markus Strobach <45330496+strobocopter@users.noreply.github.com>
2025-03-28 23:20:37 +0100

    fix: loop through all description lines to find relevant type (#461)
    Co-authored-by: Markus Strobach <markus.strobach@siemens.com>
    Co-authored-by: Arthur Fiorette <me@arthur.place>

--- src/util/create-signature.ts
+++ src/util/create-signature.ts
@@ -6,7 +6,22 @@ export function createType(
   description: string | undefined,
   config: PrismaJsonTypesGeneratorConfig
 ) {
-  const parsed = parseTypeSyntax(description);
+  if (!description) {
+    return 'unknown';
+  }
+
+  // Description can be a multi-line string like:
+  //   '@DtoCastType(Customization, ../../types, Customization)\n' +
+  //   '[CustomizationType]'
+  // So we need to find the correct line to parse
+  const lines = description.split('\n');
+  let parsed = null;
+  for (const line of lines) {
+    parsed = parseTypeSyntax(line);
+    if (parsed) {
+      break;
+    }
+  }
 
   // Defaults to unknown always, config.allowAny is handled before this function
   if (!parsed) {
