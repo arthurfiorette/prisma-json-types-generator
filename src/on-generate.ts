726c6f1aa75e74d4b33b5284419f31594cd7282f
Herberth <herberthobregon@gmail.com>
2025-04-23 11:38:04 -0600

    feat: support `prisma@6.6.0` and `prisma-client` TS generator (#470)
    Co-authored-by: Arthur Fiorette <47537704+arthurfiorette@users.noreply.github.com>

--- src/on-generate.ts
+++ src/on-generate.ts
@@ -1,3 +1,4 @@
+import { join } from 'node:path';
 import type { GeneratorOptions } from '@prisma/generator-helper';
 import ts from 'typescript';
 import { handlePrismaModule } from './handler/module';
@@ -14,11 +15,11 @@ export async function onGenerate(options: GeneratorOptions) {
 
     const config = parseConfig(options.generator.config);
 
-    const clientOutput = buildTypesFilePath(
-      prismaClient.output.value,
-      config.clientOutput,
-      options.schemaPath
-    );
+    const isNewClient =
+      (prismaClient.provider.fromEnvVar || prismaClient.provider.value) === 'prisma-client';
+    const clientOutput = isNewClient
+      ? join(prismaClient.output.value, 'client.ts')
+      : buildTypesFilePath(prismaClient.output.value, config.clientOutput, options.schemaPath);
 
     const writer = new DeclarationWriter(clientOutput, config);
 
