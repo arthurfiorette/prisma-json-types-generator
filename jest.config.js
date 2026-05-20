ba047b1694fae3e49ce30cd43ee5dbbf49e8fb74
ToTheos-Dev <ToTheos-Dev 97577314+ToTheos-Dev@users.noreply.github.com>
2026-05-20 13:53:59 +1000

    test: add test configuration

---
+++ jest.config.js
@@ -0,0 +1,16 @@
+const { createDefaultPreset } = require('ts-jest');
+
+const tsJestTransformCfg = createDefaultPreset().transform;
+
+/** @type {import("jest").Config} **/
+module.exports = {
+  testEnvironment: 'node',
+  transform: {
+    ...tsJestTransformCfg
+  },
+  testMatch: ['**/__tests__/**/*.[jt]s?(x)', '**/?(*.)+(spec|test).[jt]s?(x)'],
+  testPathIgnorePatterns: ['/node_modules/', '/dist/', '/test/unit/'],
+  moduleNameMapper: {
+    '^@/(.*)$': '<rootDir>/src/$1'
+  }
+};
